80dde290e622f4d53a3e6bba370774c44b8f7776
Luiz Passos Filho <0509luiz@gmail.com>
2020-03-17 18:45:22 -0300

    começo js

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
 /*
 - mostrar numeros clicados no display (check)
-- colocar numeros um do lado do outro ao clicar nos botoes
+- colocar numeros um do lado do outro ao clicar nos botoes (check)
 - detectar o simbolo da operação ao clicar no botao
 - fazer todas as operaçoes
 - salvar como previousOperand tudo que vier antes de uma operationKey
 - se clicar em uma operationKey pela segunda vez faz a operação e mostra o resultado no previousOperand
 - botao ac limpa previous e currentOperand
-- botao delete apagar um caracter por vez começando pelo ultimo
+- botao delete apagar um caracter por vez começando pelo ultimo (check)
 */
 
+savedNumber = document.getElementById('current-operand').innerHTML
+function visor(number) {
+  savedNumber = document.getElementById('current-operand').innerHTML
+  document.getElementById('current-operand').innerHTML = this.savedNumber + number;
 
-var clickedButton = document.getElementsByTagName("button");
-for (var i = 0; i < clickedButton.length; i++) {
-  (function (i) {
-    clickedButton[i].addEventListener("click", function () {
-      document.getElementById("current-operand").innerHTML.toString()
-      clickedButton = document.getElementsByTagName("button")[i].innerHTML
-      document.getElementById("current-operand").innerHTML = clickedButton;
-      
-       var concat = clickedButton.concat(document.getElementById("current-operand").innerHTML)
-       document.getElementById("current-operand").innerHTML = concat
-    })
-  })(i);
+
+
 }
 
 function clearAll() {
-  document.getElementById("current-operand").innerHTML = ' ';
-  document.getElementById("previous-operand").innerHTML = ' ';
+  document.getElementById("current-operand").innerHTML = '';
+  document.getElementById("previous-operand").innerHTML = '';
   console.log("clear")
 }
 
-
-
-
-
-
+function deleteThis() {
+  tamanhoDoNumber = document.getElementById('current-operand').innerHTML.length
+  document.getElementById('current-operand').innerHTML = document.getElementById('current-operand').innerHTML.slice(0,tamanhoDoNumber - 1)
+}
 
 
