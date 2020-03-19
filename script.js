b17f0f89f2982b6582e8190a87c7d67e53b88e4b
Luiz Passos Filho <0509luiz@gmail.com>
2020-03-18 22:48:27 -0300

    versão quase final

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 - mostrar numeros clicados no display (check)
 - colocar numeros um do lado do outro ao clicar nos botoes (check)
 - detectar o simbolo da operação ao clicar no botao (check)
-- fazer todas as operaçoes
+- fazer todas as operaçoes (check)
 - salvar como previousOperand tudo que vier antes de uma operationKey (check*)
-- se clicar em uma operationKey pela segunda vez faz a operação e mostra o resultado no previousOperand
+- se clicar em uma operationKey pela segunda vez faz a operação e mostra o resultado no previousOperand (check)
 - botao ac limpa previous e currentOperand (check)
 - botao delete apagar um caracter por vez começando pelo ultimo (check)
-- só permitir um ponto por numeral
+- só permitir um ponto por numeral (check)
 */
 
 savedNumber = document.getElementById('current-operand').innerHTML
@@ -31,11 +31,23 @@ function deleteThis() {
 }
 
 function updateVisor(value) {
-  document.getElementById("previous-operand").innerHTML = document.getElementById("current-operand").innerHTML + value;
-  document.getElementById("current-operand").innerHTML = '';
 
+  if (document.getElementById("previous-operand").innerHTML === '') {
+
+    document.getElementById("previous-operand").innerHTML = document.getElementById("current-operand").innerHTML + value;
+    document.getElementById("current-operand").innerHTML = '';
+
+  }
+  else {
+    var result = checkString().toString();
+    document.getElementById("previous-operand").innerHTML = result + value;
+    document.getElementById("current-operand").innerHTML = '';
+
+
+  }
 
 }
+
 
 
 
@@ -50,8 +62,9 @@ function checkString() {
     result = number1 * number2
     document.getElementById("current-operand").innerHTML = result
     document.getElementById("previous-operand").innerHTML = '';
+    return result
 
-    
+
   }
 
   var checkPlus = str.includes("+");
@@ -61,6 +74,7 @@ function checkString() {
     result = number1 + number2
     document.getElementById("current-operand").innerHTML = result
     document.getElementById("previous-operand").innerHTML = '';
+    return result
   }
 
   var checkDivide = str.includes("÷");
@@ -70,22 +84,36 @@ function checkString() {
     result = number1 / number2
     document.getElementById("current-operand").innerHTML = result
     document.getElementById("previous-operand").innerHTML = '';
+    return result
   }
 
   var checkMinus = str.includes("-");
   if (checkMinus === true) {
+
     number1 = parseFloat(document.getElementById("previous-operand").innerHTML)
     number2 = parseFloat(document.getElementById("current-operand").innerHTML)
     result = number1 - number2
     document.getElementById("current-operand").innerHTML = result
     document.getElementById("previous-operand").innerHTML = '';
+    return result
+
   }
+  if (document.getElementById("previous-operand").innerHTML === '') {
+    result = parseFloat(document.getElementById("current-operand").innerHTML)
+    return result
+  }
+  updateVisor();
 }
 
-function checkMultipleOperation(intString) {
-  
-   
+function addComma(comma) {
+  var str = document.getElementById("current-operand").innerHTML
+  var checkcomma = str.includes(".");
+  if (checkcomma === false) {
+    savedNumber = document.getElementById('current-operand').innerHTML
+    document.getElementById('current-operand').innerHTML = this.savedNumber + comma;
   }
-  
-  
-  
+
+}
+
+
+
